e3d771c054c47e8993db2eaa3a7b88eccc40729f
MarcRegas <169176400+MarcRegas@users.noreply.github.com>
2024-05-07 17:21:27 +0200

    Create contador.php

---
+++ contador.php
@@ -0,0 +1,11 @@
+﻿<?php
+    $count=$_COOKIE['count'];
+    if(isset($count)){
+            $count++;
+    }else{
+            $count=0;
+    }
+
+    setcookie('count', $count);
+    echo $count;
+?>
