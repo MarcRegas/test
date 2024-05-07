62b4a3b629a4556fa5a608a7a1631692e9917937
MarcRegas <169176400+MarcRegas@users.noreply.github.com>
2024-05-07 17:25:57 +0200

    Create contador4.php

---
+++ contador4.php
@@ -0,0 +1,21 @@
+﻿<?php
+ini set('display errors', 1); 
+ini_set('display_startup_errors", 1); 
+error_reporting(E_ALL);
+$servername = "localhost";
+$username = "admin"; 
+$password = "12345";
+$dbname "estadistics";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_errno) {
+echo "Error en connectar a MySQL: 
+exit();
+}
+$ip-$_SERVER['REMOTE_ADDR"];
+$conn->connect_error;
+$sql="INSERT INTO registre(ip) VALUES ('" . $ip . "')";
+$conn->query($sql);
+$resultat = $conn->query("SELECT COUNT(*) FROM registre"); $row = mysqli_fetch_array($resultat);
+echo $row[0];
+$conn->close();
+?>
